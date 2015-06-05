77da51a2a93b13a4c46c337572c6a6453cfd5475
Sweetness <a@fu-labs.com>
2015-06-05 11:35:38 -0400

    moved metadata struct to resp.go

--- resp.go
+++ resp.go
@@ -5,6 +5,16 @@ import "fmt"
 
 //import "encoding/json"
 
+// MetaData is a struct for metadata returned in a PBResp.Body
+type MetaData struct {
+	LastModified   string `json:"lastModifiedDate,omitempty"`
+	LastModifiedBy string `json:"lastModifiedBy,omitempty"`
+	Created        string `json:"createdDate,omitempty"` //"2014-02-01T11:12:12Z",
+	CreatedBy      string `json:"createdBy,omitempty"`
+	State          string `json:"state,omitempty"`
+	Etag           string `json:"etag,omitempty"`
+}
+
 // PBResp is the struct returned by all Rest request functions
 type PBResp struct {
 	req_url    string
