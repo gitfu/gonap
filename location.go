72724460c8ebdcc23e34214ecf79ba34e772ae16
Sweetness <a@fu-labs.com>
2015-06-05 12:38:59 -0400

    Adding location.go

---
+++ location.go
@@ -0,0 +1,55 @@
+package gonap
+
+import "encoding/json"
+
+// locations_path for urls
+const locations_path = "/locations"
+
+// LocationProperties is aa struct for Locaation Properties
+type LocationProperties struct {
+	Name string `json:"name"`
+}
+
+// Location is the struct for a Location
+type Location struct {
+	Id         string             `json:"id,omitempty"`
+	Type       string             `json:"type,omitempty"`
+	Href       string             `json:"href,omitempty"`
+	MetaData   MetaData           `json:"metadata,omitempty"`
+	Properties LocationProperties `json:"properties"`
+	Resp       PBResp             `json:"-"`
+}
+
+func toLocation(pbresp PBResp) Location {
+	var loc Location
+	json.Unmarshal(pbresp.Body, &loc)
+	loc.Resp = pbresp
+	return loc
+}
+
+// Locations is the struct for a Location Collection
+type Locations struct {
+	Id    string     `json:"id,omitempty"`
+	Type  string     `json:"type,omitempty"`
+	Href  string     `json:"href,omitempty"`
+	Items []Location `json:"items,omitempty"`
+	Resp  PBResp     `json:"-"`
+}
+
+func toLocations(pbresp PBResp) Locations {
+	var locs Locations
+	json.Unmarshal(pbresp.Body, &locs)
+	locs.Resp = pbresp
+	return locs
+}
+
+// ListLocations returns a PBResp with location collection data in the Body
+func ListLocations() Locations {
+	return toLocations(is_get(location_col_path()))
+}
+
+// GetLocation returns a PBResp with data for a location in the Body
+func GetLocation(locid string) Location {
+	path := location_path(locid)
+	return toLocation(is_get(path))
+}
