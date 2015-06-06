ba6d2ce5309638235bc3e0186e34e225acafbde6
Sweetness <a@fu-labs.com>
2015-06-06 11:32:52 -0400

    Adding lan testing, it already caught a bug too.

--- lan.go
+++ lan.go
@@ -1,11 +1,13 @@
 package gonap
 
 import "encoding/json"
+
 // Lan_Properties is  just that
 type Lan_Properties struct {
 	Name   string `json:"name,omitempty"`
 	Public bool   `json:"public,omitempty"`
 }
+
 // Lan_Entities contains a Nics struct, a collection of Nic structs
 type Lan_Entities struct {
 	Nics Nics `json:"nics,omitempty"`
@@ -19,7 +21,6 @@ type Lan struct {
 	Resp       PBResp         `json:"-"`
 }
 
-
 // toLan converts a PBResp struct into a Lan struct
 func toLan(pbresp PBResp) Lan {
 	var lan Lan
@@ -27,7 +28,6 @@ func toLan(pbresp PBResp) Lan {
 	lan.Resp = pbresp
 	return lan
 }
-
 
 type Lans struct {
 	Id_Type_Href
@@ -38,7 +38,7 @@ type Lans struct {
 func toLans(pbresp PBResp) Lans {
 	var lans Lans
 	json.Unmarshal(pbresp.Body, &lans)
-	lans.Resp =  pbresp
+	lans.Resp = pbresp
 	return lans
 }
 
@@ -67,6 +67,7 @@ func UpdateLan(dcid string, lanid string, jason []byte) Lan {
 	path := lan_path(dcid, lanid)
 	return toLan(is_put(path, jason))
 }
+
 // PatchLan does a partial update to a lan using json from []byte jason
 // returns a Lan struct
 func PatchLan(dcid string, lanid string, jason []byte) Lan {
---
+++ lan_test.go
@@ -0,0 +1,94 @@
+// lan_test.go
+package gonap
+
+import "testing"
+import "fmt"
+
+var lan_dcid = mkdcid()
+
+func mklanid(lan_dcid string) string {
+	var jason = []byte(`{
+					"name":"A Lan"
+					}`)
+	lan := CreateLan(lan_dcid, jason)
+	lanid := lan.Id
+	return lanid
+}
+
+func TestListLans(t *testing.T) {
+	t.Parallel()
+	shouldbe := "collection"
+	want := 200
+	lans := ListLans(lan_dcid)
+
+	if lans.Type != shouldbe {
+		t.Errorf("ListServers() type == %v, wanted %v", lans.Type, shouldbe)
+	}
+	if lans.Resp.StatusCode != want {
+		fmt.Println(lans.Resp.Body)
+		t.Errorf("ListServers() StatusCode == %v, wanted %v", lans.Resp.StatusCode, want)
+	}
+}
+
+func TestCreateLan(t *testing.T) {
+	t.Parallel()
+	want := 202
+	var jason = []byte(`{
+					"name":"A lan"
+					}`)
+	lan := CreateLan(lan_dcid, jason)
+	if lan.Resp.StatusCode != want {
+		t.Errorf("CreateLan() StatusCode == %v, wanted %v", lan.Resp.StatusCode, want)
+	}
+}
+
+func TestGetLan(t *testing.T) {
+	t.Parallel()
+	shouldbe := "lan"
+	want := 200
+	lanid := mklanid(lan_dcid)
+	lan := GetLan(lan_dcid, lanid)
+	if lan.Type != shouldbe {
+		t.Errorf("GetLan() type == %v, wanted %v", lan.Type, shouldbe)
+	}
+	if lan.Resp.StatusCode != want {
+		t.Errorf("GetLan() StatusCode == %v, wanted %v", lan.Resp.StatusCode, want)
+	}
+}
+
+func TestPatchLan(t *testing.T) {
+	t.Parallel()
+	want := 202
+	jason_patch := []byte(`{
+					"name":"Patched lan",
+					}`)
+	lanid := mklanid(lan_dcid)
+	lan := PatchLan(lan_dcid, lanid, jason_patch)
+	if lan.Resp.StatusCode != want {
+		t.Errorf("PatchLan() StatusCode == %v, wanted %v", lan.Resp.StatusCode, want)
+	}
+}
+
+func TestUpdateLan(t *testing.T) {
+	t.Parallel()
+	want := 202
+	jason_update := []byte(`{
+					"name":"Updated Lan"
+					}`)
+
+	lanid := mklanid(lan_dcid)
+	lan := UpdateLan(lan_dcid, lanid, jason_update)
+	if lan.Resp.StatusCode != want {
+		t.Errorf("UpdateLan() StatusCode == %v, wanted %v", lan.Resp.StatusCode, want)
+	}
+}
+
+func TestDeleteLan(t *testing.T) {
+	t.Parallel()
+	want := 202
+	lanid := mklanid(lan_dcid)
+	resp := DeleteLan(lan_dcid, lanid)
+	if resp.Resp.StatusCode != want {
+		t.Errorf("DeleteLan() StatusCode == %v, wanted %v", resp.Resp.StatusCode, want)
+	}
+}
